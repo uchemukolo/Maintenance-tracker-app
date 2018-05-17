a548da81b7433c8c81e7dc0b597f82f336db85d2
unknown <muche>
2018-05-17 19:53:34 +0100

    create fetch one request api for a logged in user

--- server/controllers/requests.js
+++ server/controllers/requests.js
@@ -10,25 +10,42 @@ const request = db;
  * @class Request
  */
 class Request {
-/**
- *@description - Fetch all the requests of a logged in user
- *
- *@param {object} request - HTTP request
- *
- * @param {object} response
- *
- * @return {object} this - Class instance
- *
- * @memberof Request
- */
+	/**
+	 *@description - Fetch all the requests of a logged in user
+ 	 *
+   *@param {object} request - HTTP request
+   *
+   * @param {object} response
+   *
+   * @return {object} this - Class instance
+   *
+   * @memberof Request
+   */
 	getAll (req,res){
 		return res.status(200).json({
 			message: 'Successful',
 			request: db,
 			error: false
 		});
-	return this;
-}
+		return this;
+	}
+	getOne(req,res){
+		for(let i=0; i < request.length; i++){
+			if(request[i].requestId === parseInt(req.params.requestId, 10)){
+				return res.json({
+					message: 'Successful',
+					request: db[i],
+					error: false
+				});
+			}
+		}
+		return res.status(404).json({
+			message: 'Request not found!',
+			error: true
+		});
+		return this;
+	}
+	
 }
 const requestController = new Request();
 export default requestController;
--- server/db/request.js
+++ server/db/request.js
@@ -1,6 +1,7 @@
 const Request = [
 	{
 		userId: 1,
+		requestId: 1,
 		title:'Faulty Laptop',
 		category: 'Repair',
     description:' My laptop is not coming up. Yesterday everything was working fine but this morning I noticed the laptop was very hot and not coming up',
@@ -9,6 +10,7 @@ const Request = [
 	},
 	{
 		userId: 2,
+		requestId: 2,
 		title:'Faulty Phone',
 		category: 'Repair',
     description:' My phone is not coming up. Yesterday everything was working fine but this morning I noticed the laptop was very hot and not coming up',
