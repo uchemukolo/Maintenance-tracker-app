0edc0f260e0b6bb9f49293473d45339476c1ed2f
unknown <muche>
2018-05-17 21:15:08 +0100

    create modify details api endpoint

--- server/db/request.js
+++ server/db/request.js
@@ -7,15 +7,15 @@ const Request = [
     description:' My laptop is not coming up. Yesterday everything was working fine but this morning I noticed the laptop was very hot and not coming up',
 		urgencyLevel:'High',
 		date: '15/05/2018'
-	},
-	{
-		userId: 2,
-		requestId: 2,
-		title:'Faulty Phone',
-		category: 'Repair',
-    description:' My phone is not coming up. Yesterday everything was working fine but this morning I noticed the laptop was very hot and not coming up',
-		urgencyLevel:'High',
-		date: '15/05/2018'
-	},
+	}
+	// {
+	// 	userId: 2,
+	// 	requestId: 2,
+	// 	title:'Faulty Phone',
+	// 	category: 'Repair',
+  //   description:' My phone is not coming up. Yesterday everything was working fine but this morning I noticed the laptop was very hot and not coming up',
+	// 	urgencyLevel:'High',
+	// 	date: '15/05/2018'
+	// },
 ]
 export default Request;
