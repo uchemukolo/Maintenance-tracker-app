b50bf849e055d0ac71b43818f183026e72f27374
unknown <muche>
2018-05-18 15:47:27 +0100

    Add test cases to test endpoint

--- server/app.js
+++ server/app.js
@@ -1,22 +1,17 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import logger from 'morgan';
-import bodyParser from 'body-parser';
 import request from './routes/request';
 
-// setup/initialize express app
 const app = express();
 
-// log requests to the console
 app.use(logger('dev'));
 
-// Parse incoming request bodies in a middleware before your handlers,
-// available under the req.body property.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/v1/users/requests', request);
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('', (req, res) => res.status(200).json({
   message: 'Welcome To Maintenance Tracker API!!!',
 }));
