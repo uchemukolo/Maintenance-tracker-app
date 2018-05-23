4d25621cea47333f0475ba6a8f5613c2fcc0323c
unknown <muche>
2018-05-23 22:03:39 +0100

    add signup route

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import logger from 'morgan';
 import request from './routes/request';
-// import user from './routes/user';
+import user from './routes/user';
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/v1/users/requests', request);
-// app.use('/api/v1/auth/signup', user);
+app.use('/api/v1/auth/signup', user);
 
 app.get('', (req, res) => res.status(200).json({
   message: 'Welcome To Maintenance Tracker API!!!',
