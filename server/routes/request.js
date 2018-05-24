2b2e02d6c2e6e46a8e062bb7c6adc6cfe98113c1
unknown <muche>
2018-05-24 21:58:57 +0100

    update request route

--- server/routes/request.js
+++ server/routes/request.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import request from '../controllers/requests';
 import Validate from '../middleware/validate';
+import Auth from '../middleware/Authenticate';
 
 const router = express.Router();
 
-router.get('/', request.getAll);
-router.get('/:requestId', request.getOne);
-router.post('/', Validate.createRequest, request.createRequest);
-router.put('/:requestId', request.modifyRequest);
+// router.get('/', request.getAll);
+// router.get('/:requestId', request.getOne);
+router.post('/', Auth.Verify, Validate.createRequest, request.createRequest);
+// router.put('/:requestId', request.modifyRequest);
 
 
 export default router;
